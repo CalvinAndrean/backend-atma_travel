8416d16e22f1fe40c811209fa33fb08f7cb14773
AdnSmile <nessafitri16@gmail.com>
2022-12-07 15:45:48 +0700

    Selesai API Rating

--- app/Http/Controllers/DestinasiController.php
+++ app/Http/Controllers/DestinasiController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Http\Resources\DestinasiResource;
-
+use App\Models\Destinasi;
 use Illuminate\Http\Request;
 
 class DestinasiController extends Controller
--- app/Http/Controllers/PlannerController.php
+++ app/Http/Controllers/PlannerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Http\Resources\PlannerResource;
+use App\Models\Planner;
 
 use Illuminate\Http\Request;
 
